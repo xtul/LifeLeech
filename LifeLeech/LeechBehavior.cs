2548ffc2311871635ff79a3878093b3f56a77a44
Michał Piasecki <michal@elexpress.pl>
2020-04-21 19:14:55 +0200

    Added try/catch to OnAgentHit/Removed. Last time Bannerlord totally ignored try/catch but I'm desperate.

--- LifeLeech/LeechBehavior.cs
+++ LifeLeech/LeechBehavior.cs
@@ -1,5 +1,6 @@
 ﻿using TaleWorlds.MountAndBlade;
 using TaleWorlds.Core;
+using System;
 
 namespace LifeLeech {
 	internal partial class LeechBehavior : MissionBehaviour {
@@ -15,61 +16,65 @@ namespace LifeLeech {
 		/// Runs when any agent in mission is hit. Includes mounts.
 		/// </summary>
 		public override void OnAgentHit(Agent affectedAgent, Agent affectorAgent, int damage, int weaponKind, int currentWeaponUsageIndex) {
-			if (config.KillBased) {
-				return;
-			}
-			if (damage == 0) {
-				Say($"{affectorAgent.Name} dealt no damage.");
-				return;
-			}
-			if (config.OnlyNamedCharacters && !affectorAgent.IsHero) {
-				Say($"{affectorAgent.Name} isn't a hero.");
-				return;
-			}
-			if (affectorAgent.Character == null) {
-				Say($"{affectorAgent.Name} isn't a character/is a mount.");
-				return;
-			}
-			if (config.ExcludeCavalry && affectorAgent.HasMount) {
-				Say($"{affectorAgent.Name} is mounted - no healing.");
-				return;
-			}
-			if (config.PlayerOnly && !affectorAgent.IsPlayerControlled) {
-				Say($"{affectorAgent.Name} isn't a player.");
-				return;
-			}
+			try {
+				if (config.KillBased) {
+					return;
+				}
+				if (damage == 0) {
+					Say($"{affectorAgent.Name} dealt no damage.");
+					return;
+				}
+				if (config.OnlyNamedCharacters && !affectorAgent.IsHero) {
+					Say($"{affectorAgent.Name} isn't a hero.");
+					return;
+				}
+				if (affectorAgent.Character == null) {
+					Say($"{affectorAgent.Name} isn't a character/is a mount.");
+					return;
+				}
+				if (config.ExcludeCavalry && affectorAgent.HasMount) {
+					Say($"{affectorAgent.Name} is mounted - no healing.");
+					return;
+				}
+				if (config.PlayerOnly && !affectorAgent.IsPlayerControlled) {
+					Say($"{affectorAgent.Name} isn't a player.");
+					return;
+				}
 
-			DoHealing(affectorAgent);
+				DoHealing(affectorAgent);
+			} catch (Exception ex) { Say($"{ex.Message}") };
 		}
 
 		/// <summary>
 		/// Runs when any agent in mission is removed in some way. Includes mounts. Includes kills, but method also handles removal by other means.
 		/// </summary>
 		public override void OnAgentRemoved(Agent affectedAgent, Agent affectorAgent, AgentState agentState, KillingBlow blow) {
-			if (!config.KillBased) { 
-				return;
-			}
-			if (!blow.IsValid) {
-				return;
-			}
-			if (affectorAgent.Character == null || affectedAgent.Character == null) {
-				Say($"{affectorAgent.Name} isn't a character/is a mount.");
-				return;
-			}
-			if (config.OnlyNamedCharacters && !affectorAgent.IsHero) {
-				Say($"{affectorAgent.Name} isn't a hero.");
-				return;
-			}
-			if (config.PlayerOnly && !affectorAgent.IsPlayerControlled) {
-				Say($"{affectorAgent.Name} isn't a player.");
-				return;
-			}
-			if (config.ExcludeCavalry && affectorAgent.HasMount) {
-				Say($"{affectorAgent.Name} is mounted - no healing.");
-				return;
-			}
+			try {
+				if (!config.KillBased) {
+					return;
+				}
+				if (!blow.IsValid) {
+					return;
+				}
+				if (affectorAgent.Character == null || affectedAgent.Character == null) {
+					Say($"{affectorAgent.Name} isn't a character/is a mount.");
+					return;
+				}
+				if (config.OnlyNamedCharacters && !affectorAgent.IsHero) {
+					Say($"{affectorAgent.Name} isn't a hero.");
+					return;
+				}
+				if (config.PlayerOnly && !affectorAgent.IsPlayerControlled) {
+					Say($"{affectorAgent.Name} isn't a player.");
+					return;
+				}
+				if (config.ExcludeCavalry && affectorAgent.HasMount) {
+					Say($"{affectorAgent.Name} is mounted - no healing.");
+					return;
+				}
 
-			DoHealing(affectorAgent);
+				DoHealing(affectorAgent);
+			} catch (Exception ex) { Say($"{ex.Message}") };
 		}
 	}
 }
